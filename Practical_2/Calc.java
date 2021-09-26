6e063aabfb695a5900d6ff791b36f28d04609e39
Anurag Kumar Jha <f20200128@dubai.bits-pilani.ac.in>
2021-09-26 18:51:08 +0400

    Update Calc.java

--- Practical_2/Calc.java
+++ Practical_2/Calc.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 class Calc {
   public static void main(String[] args) {
 
-    char operator;11
+    char operator;1111
     Double number1, number2, result;
     Scanner input = new Scanner(System.in);
     
